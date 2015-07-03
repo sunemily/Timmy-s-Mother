940274f684c328dba016da73e65c1c94bf3feabf
JohnWestwig <john.westwig@gmail.com>
2015-07-03 10:55:42 -0400

    Added functions

--- Documents/Arduino/libraries/Adafruit_Motor_Shield_V2_Library-master/DCMotorTest_bothMotors/DCMotorTest_bothMotors.ino
+++ Documents/Arduino/libraries/Adafruit_Motor_Shield_V2_Library-master/DCMotorTest_bothMotors/DCMotorTest_bothMotors.ino
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #include <Adafruit_MotorShield.h>
 #include <Wire.h>
 #include "utility/Adafruit_PWMServoDriver.h"
@@ -126,3 +127,108 @@ void turnToAngle(int angle)
   myOtherMotor->setSpeed(0);
 }
 
+=======
+/* 
+This is a test sketch for the Adafruit assembled Motor Shield for Arduino v2
+It won't work with v1.x motor shields! Only for the v2's with built in PWM
+control
+
+For use with the Adafruit Motor Shield v2 
+---->  http://www.adafruit.com/products/1438
+*/
+
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include <Servo.h>
+#include "utility/Adafruit_PWMServoDriver.h"
+
+// Create the motor shield object with the default I2C address
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+// Or, create it with a different I2C address (say for stacking)
+// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61); 
+
+// Select which 'port' M1, M2, M3 or M4. In this case, M1
+Adafruit_DCMotor *myMotor = AFMS.getMotor(2);
+// You can also make another motor on port M2
+Adafruit_DCMotor *myOtherMotor = AFMS.getMotor(3);
+Adafruit_Servo *myServo = AFMS.getMotor(9);
+
+void setup() {
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+  Serial.println("Adafruit Motorshield v2 - DC Motor test!");
+
+  AFMS.begin();  // create with the default frequency 1.6KHz
+  //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
+  
+  // Set the speed to start, from 0 (off) to 255 (max speed)
+  myMotor->setSpeed(255);
+  myMotor->run(FORWARD);
+  // turn on motor
+  myMotor->run(RELEASE);
+
+  myOtherMotor->setSpeed(255);
+  myOtherMotor->run(FORWARD);
+  // turn on motor
+  myOtherMotor->run(RELEASE);
+}
+
+void loop() {
+  //uint8_t i;
+  
+  Serial.print("tick");
+
+  //for (i=155; i<255; i++) {
+   // myMotor->setSpeed(i); 
+   // myOtherMotor->setSpeed(i); 
+    //delay(100);
+  //}
+  //for (i=255; i!=155; i--) {
+   // myMotor->setSpeed(i);  
+   // myOtherMotor->setSpeed(i); 
+   // delay(100);
+ // }
+  setServo(90);
+  turnToAngle(90);
+  delay(1000);
+  turnToAngle(-90);
+  delay(1000);
+  Serial.print("tock");
+
+  //myMotor->run(BACKWARD);
+  //myOtherMotor->run(BACKWARD);
+  //for (i=155; i<255; i++) {
+   // myMotor->setSpeed(i);  
+   // myOtherMotor->setSpeed(i);
+   // delay(100);
+  //}
+  //for (i=255; i!=155; i--) {
+  //  myMotor->setSpeed(i);  
+   // myOtherMotor->setSpeed(i);
+   // delay(100);
+  //}
+
+  Serial.print("tech");
+  //myMotor->run(RELEASE);
+  //myOtherMotor->run(RELEASE);
+  delay(1000);
+}
+void turnToAngle(int angle)
+{
+  if (angle > 0)
+  {
+    myMotor->run(BACKWARD);
+    myOtherMotor->run(FORWARD);
+  }
+  else
+  {
+    myMotor->run(FORWARD);
+    myOtherMotor->run(BACKWARD);
+  }
+  myMotor->setSpeed(255);
+  myOtherMotor->setSpeed(255);
+  delay(10.5 * abs(angle));
+  myMotor->setSpeed(0);
+  myOtherMotor->setSpeed(0);
+}
+
+>>>>>>> Stashed changes
